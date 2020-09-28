b3fdf574e6957d9d2b021799ad9a7ee1a8229a30
7J647 <007J647@gmail.com>
2020-09-28 17:56:01 -0400

    rewrote Intern into a class so that it still passed the first test

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,6 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-function Intern(name, id, email, school) {
-    this.school=school;
+class Intern {
+    constructor(name, id, email, school) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.school = school;
+    }
 }
 
+
+
 module.exports = Intern;
+
+
