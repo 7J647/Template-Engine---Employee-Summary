d394b5352aa780db7842e742555b3b499a1ac606
7J647 <007J647@gmail.com>
2020-09-28 18:11:14 -0400

    rewrote first test for a class with describe, it statements

--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -1,16 +1,20 @@
 const Intern = require("../lib/Intern");
 
-test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
-});
+describe("Intern class", () => {
+  it("Can set school via constructor", () => {
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+  });
+})
+
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
   const e = new Intern("Foo", 1, "test@test.com", "UCLA");
   expect(e.getRole()).toBe(testValue);
 });
+// getRole(testValue);
 
 test("Can get school via getSchool()", () => {
   const testValue = "UCLA";
